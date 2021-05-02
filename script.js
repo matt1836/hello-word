a11b2b0659e14555ad6482b5faf7627802b84011
matt1836 <>
2021-05-02 15:28:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.write("hello world");
+document.getElementById("demo").style.textAlign = "center";
+document.getElementById("myH2").style.color = "blue";
+
+  var x = document.getElementById("myImg").src;
